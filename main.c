797b62422e3b6adaa938603489e1b01445f338a5
hisakiyo <hisakiyo.games@gmail.com>
2023-05-22 16:28:17 +0200

    switch ok

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
-
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void){
+int main(void) {
     CLKSEL0 = 0600010101;
     CLKSEL1 = 0b00001111;
     CLKPR = 0b10000000;
     CLKPR = 0;
-
-    DDRB |= 0x01;
-    PORTB |= 0x01;
     
-    while(1){
-        if((PINB & (1<<PB1)) == 0){
-            PORTB &= ~0x01;
-        }
-        else{
-            PORTB |= 0x01;
+    DDRB = 0b00000001;
+    PORTB = 0b00000001;
+
+    DDRD = 0b00000000;
+    PORTD = 0b00000000;
+    
+    while (1) {
+        if (PIND & 0b00000001) {
+            PORTB = 0b00000001;
+        } else {
+            PORTB = 0b00000000;
         }
     }
 }
+
